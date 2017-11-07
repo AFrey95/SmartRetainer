185d444e3432280333d5efb50b2ffea927e92301
Austin <abergman2@udayton.edu>
2017-11-07 11:18:53 -0500

    Added functionality to average previous 10 samples for temperature measurement

--- Arduino/SmartRetainer_arduino/temp_sensor.ino
+++ Arduino/SmartRetainer_arduino/temp_sensor.ino
@@ -10,14 +10,32 @@ float temp_sensor(int apin){
     float R; // Measured Thermistor Resistance
     int Ra = 10000; // fixed resistor used in voltage divider circuit
     float B = 3428; // B constant for 25 to 80 degrees C for NXFT15H103FA
-    int vdig = analogRead(apin); // reads in value between 0 and 1024 for input voltage
     float v_source = 5.00; // source voltage used for circuit
+    int vdig; // stores value between 0 and 1024 for input voltage
+    int sumvdig = 0; 
+    //vdig = analogRead(apin);
+    
+    // calculate average of last number of samples
+    int numsamples = 10;
+    int buf[numsamples];
+    for(int i = 1; i<= numsamples; i++)
+    {
+       buf[i] = analogRead(apin); // reads in value between 0 and 1024 for input voltage
+    }
+    
+    for(int j = 1; j<=numsamples; j++) // add up all the samples
+    {
+       sumvdig = sumvdig + buf[j]; 
+    }
+    vdig = sumvdig/numsamples; //divide by number of samples to get average
+    
     
     // convert reference voltage value to actual sensed voltage
     float v_sense;
-   
     v_sense = (vdig * v_source) / 1024; 
    
+
+    
     
     // calculate R
     R = v_sense/(v_source-v_sense)*Ra;
@@ -33,5 +51,3 @@ float temp_sensor(int apin){
     
     return tempF;
 } 
-
-
