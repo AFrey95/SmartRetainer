0a1f10774d9a30c52d1ad1847bdf05942950077c
Austin <abergman2@udayton.edu>
2017-11-07 10:50:40 -0500

    Correct temperature sensing equation as it is supposed to use Kelvin and not celcius

--- Arduino/SmartRetainer_arduino/temp_sensor.ino
+++ Arduino/SmartRetainer_arduino/temp_sensor.ino
@@ -1,9 +1,11 @@
 #include <math.h>
 
-double temp_sensor(int apin){
+float temp_sensor(int apin){
     float tempF; // output temperature in degrees Farenheit
     float tempC; // output temperature in degrees Celcius
+    float tempK; // output temperature in Kelvin
     float temp0 = 25; // 25 degrees C
+    float temp0K = 273.15 + temp0; // 25 deg C converted to Kelvin
     int R0 = 10000; // Thermistor resistance at 25 degrees C
     float R; // Measured Thermistor Resistance
     int Ra = 10000; // fixed resistor used in voltage divider circuit
@@ -20,8 +22,11 @@ double temp_sensor(int apin){
     // calculate R
     R = v_sense/(v_source-v_sense)*Ra;
     
-    // calculate temperature in Celcius
-    tempC = 1/((1/temp0)+(1/B*log(R/R0)));
+    // calculate temperature in Kelvin
+    tempK = 1/((1/temp0K)+(1/B*log(R/R0)));
+    
+    // convert Kelvin to Celcius
+    tempC = tempK - 273.15;
     
     // convert Celcius to Farenheit
     tempF = tempC*1.8 + 32;
